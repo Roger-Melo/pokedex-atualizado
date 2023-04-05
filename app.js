a4bf99a9f394cf51ac1d92bc52358eb02ecc60ca
Roger-Melo <rogerw592@gmail.com>
2023-04-05 09:13:26 -0300

    Add initial files

---
+++ app.js
@@ -0,0 +1,20 @@
+const getTypeColor = type => {
+  const normal = '#F5F5F5'
+  return {
+    normal,
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    ice: '#DEF3FD',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    ghost: '#cac0f7',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    fighting: '#E6E0D4'
+  }[type] || normal
+}
