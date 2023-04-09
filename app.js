460eb56f77f10a00e0c395cb143f694b9f72c907
Roger Melo <rogerw592@gmail.com>
2023-04-09 13:02:52 -0300

    Change HEX to uppercase

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const getTypeColor = type => {
     electric: '#FCF7DE',
     ice: '#DEF3FD',
     water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    ghost: '#cac0f7',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
+    ground: '#F4E7DA',
+    rock: '#D5D5D4',
+    fairy: '#FCEAFF',
+    poison: '#98D7A5',
+    bug: '#F8D5A3',
+    ghost: '#CAC0F7',
+    dragon: '#97B3E6',
+    psychic: '#EAEDA1',
     fighting: '#E6E0D4'
   }[type] || normal
 }
